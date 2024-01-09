836ca39e8f6a89be9c6b0e63ab5a33329af8286b
khlee0192 <khlee0192@gmail.com>
2024-01-09 10:36:37 +0900

    240109 added stepLR

--- code/pipeline_rf.py
+++ code/pipeline_rf.py
@@ -17,6 +17,7 @@ from typing import Any, Callable, Dict, List, Optional, Union
 import copy
 
 import torch
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 from packaging import version
 from transformers import CLIPImageProcessor, CLIPTextModel, CLIPTokenizer
 
@@ -838,15 +839,25 @@ class RectifiedInversableFlowPipeline(RectifiedFlowPipeline):
             current_latents, 
             t, dt, prompt_embeds,
             do_classifier_free_guidance,
-            verbose=True):
+            verbose=True,
+            warmup = False,
+            warmup_time = 0,
+            original_step_size = 0.1,
+            factor=0.5, patience=20, th=1e-3
+            ):
         """
         The forward step method assumes that current_latents are at right place(even on multistep), then map latents correctly to current latents
         The work is done by fixed-point iteration
         """
 
         latents_s = latents
+        step_scheduler = StepScheduler(current_lr=step_size, factor=factor, patience=patience)
 
         for i in range(200):
+            if warmup:
+                    if i < warmup_time:
+                        step_size = original_step_size * (i+1)/(warmup_time)
+
             latent_model_input = torch.cat([latents_s] * 2) if do_classifier_free_guidance else latents_s
             vec_t = torch.ones((latent_model_input.shape[0],), device=latents.device) * t
             v_pred = self.unet(latent_model_input, vec_t, encoder_hidden_states=prompt_embeds).sample
@@ -857,6 +868,14 @@ class RectifiedInversableFlowPipeline(RectifiedFlowPipeline):
             
             latents_t = latents_s + dt * v_pred
 
+            loss = torch.nn.functional.mse_loss(latents_t, current_latents, reduction='sum')
+            if loss.item() < th:
+                break
+
+            latents_s = latents_s - step_size * (latents_t - current_latents)
+            step_size = step_scheduler.step(loss)
+
+            """
             # Update
             if i < 50:
                 latents_s = latents_s - 0.1 * (latents_t - current_latents)
@@ -864,8 +883,75 @@ class RectifiedInversableFlowPipeline(RectifiedFlowPipeline):
                 latents_s = latents_s - 0.01 * (latents_t - current_latents)
             else:
                 latents_s = latents_s - 0.001 * (latents_t - current_latents)
-
+            """
+                
             if verbose:
                 print(i, (latents_t - current_latents).norm()/current_latents.norm(), latents_s.mean().item(), latents_s.std().item())
 
         return latents_s
+    
+class StepScheduler(ReduceLROnPlateau):
+    def __init__(self, mode='min', current_lr=0, factor=0.1, patience=10,
+                 threshold=1e-4, threshold_mode='rel', cooldown=0,
+                 min_lr=0, eps=1e-8, verbose=False):
+        if factor >= 1.0:
+            raise ValueError('Factor should be < 1.0.')
+        self.factor = factor
+        if current_lr == 0:
+            raise ValueError('Step size cannot be 0')
+
+        self.min_lr = min_lr
+        self.current_lr = current_lr
+        self.patience = patience
+        self.verbose = verbose
+        self.cooldown = cooldown
+        self.cooldown_counter = 0
+        self.mode = mode
+        self.threshold = threshold
+        self.threshold_mode = threshold_mode
+        self.best = None
+        self.num_bad_epochs = None
+        self.mode_worse = None  # the worse value for the chosen mode
+        self.eps = eps
+        self.last_epoch = 0
+        self._init_is_better(mode=mode, threshold=threshold,
+                             threshold_mode=threshold_mode)
+        self._reset()
+
+    def step(self, metrics, epoch=None):
+        # convert `metrics` to float, in case it's a zero-dim Tensor
+        current = float(metrics)
+        if epoch is None:
+            epoch = self.last_epoch + 1
+        else:
+            import warnings
+            warnings.warn("EPOCH_DEPRECATION_WARNING", UserWarning)
+        self.last_epoch = epoch
+
+        if self.is_better(current, self.best):
+            self.best = current
+            self.num_bad_epochs = 0
+        else:
+            self.num_bad_epochs += 1
+
+        if self.in_cooldown:
+            self.cooldown_counter -= 1
+            self.num_bad_epochs = 0  # ignore any bad epochs in cooldown
+
+        if self.num_bad_epochs > self.patience:
+            self._reduce_lr(epoch)
+            self.cooldown_counter = self.cooldown
+            self.num_bad_epochs = 0
+
+        return self.current_lr
+
+    def _reduce_lr(self, epoch):
+        old_lr = self.current_lr
+        new_lr = max(self.current_lr * self.factor, self.min_lr)
+        if old_lr - new_lr > self.eps:
+            self.current_lr = new_lr
+            if self.verbose:
+                epoch_str = ("%.2f" if isinstance(epoch, float) else
+                            "%.5d") % epoch
+                print('Epoch {}: reducing learning rate'
+                        ' to {:.4e}.'.format(epoch_str,new_lr))
